8e8bcf7f6cc4a29fa149716cccecd52156c557f7
kdicari24@gmail.com <kdicari24@gmail.com>
2022-12-07 11:51:36 -0500

    fixed merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,21 +1,11 @@
 const express = require('express');
-<<<<<<< HEAD
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-const db = require('./config/connection');
-// const routes = require('./routes');
-=======
 const { typeDefs, resolvers } = require('./schemas');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
-const path = require('./config/connection');
->>>>>>> 9efe9566c276aad37c9d9ba341ef0f4c21ce677e
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-<<<<<<< HEAD
-=======
 const app = express();
->>>>>>> 9efe9566c276aad37c9d9ba341ef0f4c21ce677e
 const server = new ApolloServer({
   typeDefs,
   resolvers,
